df00376f55444324c2139380dfb22141ad8ac0ea
MiguelRcol <220428103+MiguelRcol@users.noreply.github.com>
2026-07-26 17:43:17 -0500

    Create Todo data model

---
+++ src/modules/Todo.js
@@ -0,0 +1,19 @@
+class Todo {
+  constructor(title, description, dueDate, priority, notes) {
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+    this.notes = notes;
+
+    this.id = crypto.randomUUID();
+    this.completed = false;
+    this.createdAt = new Date();
+  }
+
+  toggleComplete() {
+    this.completed = !this.completed;
+  }
+}
+
+export default Todo;
